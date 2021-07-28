b286340295532a6ff7edb712a5c882ff800e4433
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2021-07-28 14:34:14 +0530

    Add files via upload

---
+++ AutoCertificate/certificate.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+from PIL import Image,ImageDraw,ImageFont
+df=pd.read_csv('names.csv')
+font=ImageFont.truetype('arial.ttf',60)
+for index,j in df.iterrows():
+    img=Image.open('Image.jpg')
+    draw=ImageDraw.Draw(img)
+    draw.text(xy=(725,760),text='{}'.format(j['name']),fill=(0,0,0),font=font)
+    img.save('pictures/{}.jpg'.format(j['name']))
